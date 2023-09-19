13cd1b6d260e63fb7f2b8044be7c1e0ad78b909f
Arthur Mateu <arthurmateu@gmail.com>
2023-09-19 11:14:57 -0300

    Completed Chapter 8 Exercises

---
+++ Chapter 8/Exercises/exercise-11.c
@@ -0,0 +1,11 @@
+/*
+    char checker_board[8][8] = {};
+
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+            ((i + j) % 2 == 0)
+            ? checker_board[i][j] = B
+            : checker_board[i][j] = R;
+        }
+    }
+*/
---
+++ Chapter 8/Exercises/exercise-9.c
@@ -0,0 +1,15 @@
+/*
+    #define DAYS_IN_MONTH 30
+    #define HOURS_IN_DAY 24
+
+    float temperature_readings[DAYS_IN_MONTH][HOURS_IN_DAY] = {...}
+    float average;
+
+    for (int i = 0; i < DAYS_IN_MONTH; i++){
+        for (int j = 0; j < HOURS_IN_DAY; j++){
+            average += temperature_readings[i][j];
+        }
+        printf("Monthly average: %.2f", temperature readings);
+        average = 0.00f;
+    }
+*/
