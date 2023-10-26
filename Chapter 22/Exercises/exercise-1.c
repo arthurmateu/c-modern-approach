d70388cd0f5d071903ce97f50dfe3dbb96897100
Arthur Mateu <arthurmateu@gmail.com>
2023-10-26 00:03:49 -0300

    Completed Chapter 22 Exercises 1-3

---
+++ Chapter 22/Exercises/exercise-1.c
@@ -0,0 +1,6 @@
+/*
+    (a) Binary data.
+    (b) Text data.
+    (c) Text data.
+    (d) Binary data.
+*/
---
+++ Chapter 22/Exercises/exercise-3.c
@@ -0,0 +1,3 @@
+/*
+    There isn't any error detection if the file can't be opened, which will give a null pointer. An easy fix would be adding an else statement, with an exit state.
+*/
